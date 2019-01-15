18bb0995e2328128fd1ccf66f8b50f92cc512722
Anthony <clan_snk_01@outlook.com>
2019-01-15 17:47:09 -0500

    Sol: Parameter must be an array

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -275,6 +275,7 @@ class RegisterController extends Controller
                         ->where('token_device','=',$request->tokenDevice)
                         ->where('sesion','=','1')
                         ->first();
+                        
         if (!empty($deviceSesion) && count($deviceSesion)>0) {
             Device::where('id',$deviceSesion->id)->update(['sesion' => 0]);
             return 1;
