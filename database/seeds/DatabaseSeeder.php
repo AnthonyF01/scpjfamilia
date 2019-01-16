0d23a53d3f9446b979fb1e3d2cb694d73456d16f
Anthony Flores <clan_snk_01@outlook.com>
2019-01-16 04:14:29 -0500

    Add Graph

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,26 +11,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(TbldepartamentoTableSeeder::class);
-        // $this->call(TblmoduloTableSeeder::class);
-        // $this->call(TblprovinciaTableSeeder::class);
-        // $this->call(TbldistritoTableSeeder::class);
-        // $this->call(TblcentrosaludTableSeeder::class);
-        // $this->call(TblcomisariaTableSeeder::class);
-        // $this->call(TblinstanciaTableSeeder::class);
-        // $this->call(TblmotivoTableSeeder::class);
-        // $this->call(TbldocumentoTableSeeder::class);
-        // $this->call(TbltipoTableSeeder::class);
-        // $this->call(TblparentescoTableSeeder::class);
-        // $this->call(TbldenunciaTableSeeder::class);
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(PermissionsTableSeeder::class);
+        $this->call(TbldepartamentoTableSeeder::class);
+        $this->call(TblmoduloTableSeeder::class);
+        $this->call(TblprovinciaTableSeeder::class);
+        $this->call(TbldistritoTableSeeder::class);
+        $this->call(TblcentrosaludTableSeeder::class);
+        $this->call(TblcomisariaTableSeeder::class);
+        $this->call(TblinstanciaTableSeeder::class);
+        $this->call(TblmotivoTableSeeder::class);
+        $this->call(TbldocumentoTableSeeder::class);
+        $this->call(TbltipoTableSeeder::class);
+        $this->call(TblparentescoTableSeeder::class);
+        $this->call(TbldenunciaTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
 
         // fake data
-        $this->call(VictimaTableSeeder::class);
-        $this->call(AgresorTableSeeder::class);
-        $this->call(DenunciaTableSeeder::class);
-        $this->call(DenunciaVictimaTableSeeder::class);
-        $this->call(DenunciaAgresorTableSeeder::class);
+        // $this->call(VictimaTableSeeder::class);
+        // $this->call(AgresorTableSeeder::class);
+        // $this->call(DenunciaTableSeeder::class);
+        // $this->call(DenunciaVictimaTableSeeder::class);
+        // $this->call(DenunciaAgresorTableSeeder::class);
     }
 }
