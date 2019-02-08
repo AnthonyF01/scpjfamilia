ac28f450221b57841d29c10109305ef53e30e549
Anthony Flores <clan_snk_01@outlook.com>
2019-02-07 21:38:57 -0500

    mas cambios

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,10 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function array_push_assoc($array, $key, $value){
+        $array[$key] = $value;
+        return $array;
+    }
+    
 }
