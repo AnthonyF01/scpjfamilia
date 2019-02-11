1fc9d8617da940e52a6beffd0cf2b16316a2f4ed
Anthony Flores <clan_snk_01@outlook.com>
2019-02-10 21:01:01 -0500

    add_column_tbldenuncia_id

---
+++ database/migrations/2019_02_10_205415_add_denuncia_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDenunciaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('denuncia', function (Blueprint $table) {
+            $table->integer('tbldenuncia_id')->unsigned()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('denuncia', function (Blueprint $table) {
+            $table->foreign('tbldenuncia_id')->references('id')->on('tbldenuncia');
+        });
+    }
+}
