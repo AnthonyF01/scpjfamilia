315e82bf89c181c947bf8a224ba6af88c6b0f295
Anthony Flores <clan_snk_01@outlook.com>
2019-05-23 20:59:41 -0500

    cambios2305

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,10 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 ini_set('MAX_EXECUTION_TIME',3600);
+ini_set('memory_limit','512M');
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
@@ -25,5 +28,4 @@ class Controller extends BaseController
        return preg_replace('/[^A-Za-z0-9\-]/', '', $string);
     }
 
-    
 }
