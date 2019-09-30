0798f604d86c946e4a0c1fb0a70ac257d9824e2e
Anthony Flores <clan_snk_01@outlook.com>
2019-09-30 08:53:40 -0500

    cambios 30/09

--- app/Http/Controllers/DenunciaController.php
+++ app/Http/Controllers/DenunciaController.php
@@ -761,7 +761,7 @@ class DenunciaController extends Controller
             }
         }
 
-        if (!empty($arrUser) && isset($arrUser[0]['user_id']) && count($arrUser) > 0) {
+        if (!empty($arrUser) && isset($arrUser[0]) && count($arrUser) > 0) {
             $notifications = DB::table('notification')
                              ->join('users as u','u.id','=','notification.user_id')
                              ->leftJoin('users as u1','u1.id','=','notification.worker_id')
