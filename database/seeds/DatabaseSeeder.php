a04c05dae23c282a88337ac3c78f55b0e6672d81
Nejer <nejerludovid@gmail>
2020-05-06 05:38:54 -0500

    Formulario Web

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,45 +11,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(TbldepartamentoTableSeeder::class);
-        $this->call(TblmoduloTableSeeder::class);
-        $this->call(TblprovinciaTableSeeder::class);
-        $this->call(TbldistritoTableSeeder::class);
-        $this->call(TblcentrosaludTableSeeder::class);
-        $this->call(TblcomisariaTableSeeder::class);
-        $this->call(TblinstanciaTableSeeder::class);
-        $this->call(TblmotivoTableSeeder::class);
-        $this->call(TbldocumentoTableSeeder::class);
-        $this->call(TbltipoTableSeeder::class);
-        $this->call(TblparentescoTableSeeder::class);
-        $this->call(TbldenunciaTableSeeder::class);
-        $this->call(TblmedidaTableSeeder::class);
-        $this->call(TbldpenalTableSeeder::class);
-        $this->call(TblviolenciaTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(PermissionsTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
-        $this->call(PermissionRoleTableSeeder::class);
-        $this->call(RoleUserTableSeeder::class);
+        // $this->call(TbldepartamentoTableSeeder::class);
+        // $this->call(TblmoduloTableSeeder::class);
+        // $this->call(TblprovinciaTableSeeder::class);
+        // $this->call(TbldistritoTableSeeder::class);
+        // $this->call(TblcentrosaludTableSeeder::class);
+         $this->call(TblcomisariaTableSeeder::class);
+        // $this->call(TblinstanciaTableSeeder::class);
+        // $this->call(TblmotivoTableSeeder::class);
+        // $this->call(TbldocumentoTableSeeder::class);
+        // $this->call(TbltipoTableSeeder::class);
+        // $this->call(TblparentescoTableSeeder::class);
+        // $this->call(TbldenunciaTableSeeder::class);
+        // $this->call(TblmedidaTableSeeder::class);
+        // $this->call(TbldpenalTableSeeder::class);
+        // $this->call(TblviolenciaTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
+        // $this->call(PermissionsTableSeeder::class);
+        // $this->call(RolesTableSeeder::class);
+        // $this->call(PermissionRoleTableSeeder::class);
+        // $this->call(RoleUserTableSeeder::class);
 
-        // datos reales
-        $this->call(VictimaTableSeeder::class);
-        $this->call(AgresorTableSeeder::class);
-        $this->call(DenunciaTableSeeder::class);
-        $this->call(DenunciaAgresorTableSeeder::class);
-        $this->call(DenunciaVictimaTableSeeder::class);
-
-        // fake data (Factory Faker)
+        // // datos reales
         // $this->call(VictimaTableSeeder::class);
         // $this->call(AgresorTableSeeder::class);
         // $this->call(DenunciaTableSeeder::class);
-        // $this->call(DenunciaVictimaTableSeeder::class);
         // $this->call(DenunciaAgresorTableSeeder::class);
+        // $this->call(DenunciaVictimaTableSeeder::class);
+
+        // // fake data (Factory Faker)
+        // // $this->call(VictimaTableSeeder::class);
+        // // $this->call(AgresorTableSeeder::class);
+        // // $this->call(DenunciaTableSeeder::class);
+        // // $this->call(DenunciaVictimaTableSeeder::class);
+        // // $this->call(DenunciaAgresorTableSeeder::class);
         
-        $this->call(DenunciaTblviolenciaTableSeeder::class);
-        $this->call(DenunciaTblmedidaTableSeeder::class);
-        // $this->call(DenunciaTbldenunciaTableSeeder::class);
-        $this->call(DocumentoTableSeeder::class);
-        // $this->call(PermissionUserTableSeeder::class);
+        // $this->call(DenunciaTblviolenciaTableSeeder::class);
+        // $this->call(DenunciaTblmedidaTableSeeder::class);
+        // // $this->call(DenunciaTbldenunciaTableSeeder::class);
+        // $this->call(DocumentoTableSeeder::class);
+        // // $this->call(PermissionUserTableSeeder::class);
     }
 }
